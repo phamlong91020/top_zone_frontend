49c3c68eed465a9e096f0936085ef4077c6815a4
phamlong91020 <phamlong9102@gmail.com>
2024-08-20 14:37:41 +0700

    fix: delete var unused

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,6 +1,5 @@
 import TOP_ZONE_LOGO from '@assets/images/topzone.jpg';
 import TOP_ZONE_BACKGROUND from '@assets/images/topzone_background.jpg';
-import BACKGROUND_AUTH from '@assets/images/background_auth.svg';
 import IMAGE_PROFILE from '@assets/images/profile.png';
 import BANNER_01 from '@assets/images/banner_01.webp';
 import BANNER_02 from '@assets/images/banner_02.webp';
@@ -82,7 +81,6 @@ import TEKZONE_08 from '@assets/images/tekzone_08.jpg';
 export default {
   TOP_ZONE_LOGO,
   TOP_ZONE_BACKGROUND,
-  BACKGROUND_AUTH,
   IMAGE_PROFILE,
   BANNER_01,
   BANNER_02,
